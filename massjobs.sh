61c189b48052ea4adb41deaaa8fa0e4fcdc8b5fa
Sarah <eno@umd.edu>
2016-12-17 17:47:19 -0500

    ha

--- massjobs.sh
+++ massjobs.sh
@@ -4,14 +4,16 @@
 cat condor-jobs.jdl \
 | sed -e s~MYFILENAME~X_d_1000_pi_2_lt0p001.txt~ \
 > condor_jobs_1.jdl
-
 condor_submit condor_jobs_1.jdl
-
 
 cat condor-jobs.jdl \
 | sed -e s~MYFILENAME~X_d_800_pi_2_lt0p001.txt~ \
 > condor_jobs_2.jdl
+condor_submit condor_jobs_2.jdl
 
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1500_pi_2_lt0p001.txt~ \
+> condor_jobs_2.jdl
 condor_submit condor_jobs_2.jdl
 
 
