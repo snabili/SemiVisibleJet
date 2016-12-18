3919a1e4a2e476cbf6c77b44aaf96365654a891b
Sarah <eno@umd.edu>
2016-12-18 10:11:34 -0500

    more points

--- massjobs.sh
+++ massjobs.sh
@@ -2,12 +2,6 @@
 
 
 cat condor-jobs.jdl \
-| sed -e s~MYFILENAME~X_d_1000_pi_2_lt0p001.txt~ \
-| sed -e s~MYPREFIX~X_d_1000_pi_2_lt0p001~ \
-> condor_jobs_1.jdl
-condor_submit condor_jobs_1.jdl
-
-cat condor-jobs.jdl \
 | sed -e s~MYFILENAME~X_d_800_pi_2_lt0p001.txt~ \
 | sed -e s~MYPREFIX~X_d_800_pi_2_lt0p001~ \
 > condor_jobs_2.jdl
@@ -18,6 +12,49 @@ cat condor-jobs.jdl \
 | sed -e s~MYPREFIX~X_d_1500_pi_2_lt0p001~ \
 > condor_jobs_2.jdl
 condor_submit condor_jobs_2.jdl
+
+
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1000_pi_2_lt0p001.txt~ \
+| sed -e s~MYPREFIX~X_d_1000_pi_2_lt0p001~ \
+> condor_jobs_1.jdl
+condor_submit condor_jobs_1.jdl
+
+
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1000_pi_2_lt0p1.txt~ \
+| sed -e s~MYPREFIX~X_d_1000_pi_2_lt0p1~ \
+> condor_jobs_1.jdl
+condor_submit condor_jobs_1.jdl
+
+
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1000_pi_2_lt1.txt~ \
+| sed -e s~MYPREFIX~X_d_1000_pi_2_lt1~ \
+> condor_jobs_1.jdl
+condor_submit condor_jobs_1.jdl
+
+
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1000_pi_2_lt5.txt~ \
+| sed -e s~MYPREFIX~X_d_1000_pi_2_lt5~ \
+> condor_jobs_1.jdl
+condor_submit condor_jobs_1.jdl
+
+
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1000_pi_2_lt150.txt~ \
+| sed -e s~MYPREFIX~X_d_1000_pi_2_lt150~ \
+> condor_jobs_1.jdl
+condor_submit condor_jobs_1.jdl
+
+
+cat condor-jobs.jdl \
+| sed -e s~MYFILENAME~X_d_1000_pi_2_lt300.txt~ \
+| sed -e s~MYPREFIX~X_d_1000_pi_2_lt300~ \
+> condor_jobs_1.jdl
+condor_submit condor_jobs_1.jdl
+
 
 
 END_TIME=`/bin/date`
